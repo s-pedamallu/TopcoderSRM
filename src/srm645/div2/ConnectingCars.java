7f903ab5eee74c765767a9aebd8b9b5743c03952
Shashank Pedamallu <pedamallush@gmail.com>
2015-01-24 22:37:07 -0500

    Solved SRM645|Div2|500

--- src/srm645/div2/ConnectingCars.java
+++ src/srm645/div2/ConnectingCars.java
@@ -1,5 +1,8 @@
 package srm645.div2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -49,84 +52,49 @@ public class ConnectingCars {
 		return minEnergy;
 	}
 	
-	public static void main(String[] args)
+	/*
+	 * Test Cases:
+	 * Input:
+	 * {1 3 10 20}
+	 * {2 2 5 3}
+	 * Desired Output:
+	 * 15
+	 * 
+	 * Input:
+	 * {100 50 1}
+	 * {10 2 1}
+	 * Desired Output:
+	 * 96
+	 * 
+	 * Input:
+	 * {4 10 100, 13 80}
+	 * {5 3 42 40 9}
+	 * Desired Output:
+	 * 66
+	 * 
+	 * Input:
+	 * {5606451 63581020 81615191 190991272 352848147 413795385 468408016 615921162 760622952 791438427}
+	 * {42643329 9909484 58137134 99547272 39849232 15146704 144630245 604149 15591965 107856540}
+	 * Desired Output:
+	 * 1009957100
+	 * */
+	
+	public static void main(String[] args) throws IOException
 	{
-		long time;
-		long answer;
-		boolean errors = false;
-		long desiredAnswer;
-		
-//		<%:start-tests%>
-		time = System.currentTimeMillis();
-		answer = new ConnectingCars().minimizeCost(new int[]{1, 3, 10, 20}, new int[]{2, 2, 5, 3});
-		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
-		desiredAnswer = 15L;
-		System.out.println("Your answer:");
-		System.out.println("\t" + answer);
-		System.out.println("Desired answer:");
-		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer)
-		{
-			errors = true;
-			System.out.println("DOESN'T MATCH!!!!");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String[] tokens = input.split(" ");
+		int[] pos = new int[tokens.length];
+		for (int i=0; i<tokens.length; i++) {
+			pos[i] = Integer.valueOf(tokens[i]);
 		}
-		else
-			System.out.println("Match :-)");
-		System.out.println();
-		time = System.currentTimeMillis();
-		answer = new ConnectingCars().minimizeCost(new int[]{100, 50, 1}, new int[]{10, 2, 1});
-		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
-		desiredAnswer = 96L;
-		System.out.println("Your answer:");
-		System.out.println("\t" + answer);
-		System.out.println("Desired answer:");
-		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer)
-		{
-			errors = true;
-			System.out.println("DOESN'T MATCH!!!!");
+		input = br.readLine();
+		tokens = input.split(" ");
+		int[] len = new int[tokens.length];
+		for (int i=0; i<tokens.length; i++) {
+			len[i] = Integer.valueOf(tokens[i]);
 		}
-		else
-			System.out.println("Match :-)");
-		System.out.println();
-		time = System.currentTimeMillis();
-		answer = new ConnectingCars().minimizeCost(new int[]{4, 10, 100, 13, 80}, new int[]{5, 3, 42, 40, 9});
-		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
-		desiredAnswer = 66L;
-		System.out.println("Your answer:");
-		System.out.println("\t" + answer);
-		System.out.println("Desired answer:");
-		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer)
-		{
-			errors = true;
-			System.out.println("DOESN'T MATCH!!!!");
-		}
-		else
-			System.out.println("Match :-)");
-		System.out.println();
-		time = System.currentTimeMillis();
-		answer = new ConnectingCars().minimizeCost(new int[]{5606451, 63581020, 81615191, 190991272, 352848147, 413795385, 468408016, 615921162, 760622952, 791438427}, new int[]{42643329, 9909484, 58137134, 99547272, 39849232, 15146704, 144630245, 604149, 15591965, 107856540});
-		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
-		desiredAnswer = 1009957100L;
-		System.out.println("Your answer:");
-		System.out.println("\t" + answer);
-		System.out.println("Desired answer:");
-		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer)
-		{
-			errors = true;
-			System.out.println("DOESN'T MATCH!!!!");
-		}
-		else
-			System.out.println("Match :-)");
-		System.out.println();
-//		<%:end-tests%>
-		
-		if (errors)
-			System.out.println("Some of the test cases had errors :-(");
-		else
-			System.out.println("You're a stud (at least on the test data)! :-D ");
+		System.out.println(new ConnectingCars().minimizeCost(pos, len));
 	}
 
 
